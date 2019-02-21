2a85e71ae30d00bbcba5763ae53ceeb7ade61b4f
Ronnie Isurina <ronnie.isurina@gmail.com>
2019-02-21 12:13:47 +0800

    PSR-2

--- app/Http/Controllers/TimeEntriesController.php
+++ app/Http/Controllers/TimeEntriesController.php
@@ -13,51 +13,49 @@ use Illuminate\Support\Facades\Log;
 
 class TimeEntriesController extends Controller
 {
-	public function index()
+     public function index()
      {
           $project_id = \Auth::user()->project_id;
-          $rs = DB::select("SELECT * FROM tbl_credit_logs WHERE project_id = ".$project_id ." ORDER BY date desc");
-          $time = DB::select("SELECT (sum(spent) - sum(spent * discount_percent / 100)) as spent FROM tbl_credit_logs WHERE project_id = ".$project_id);
+          $rs = DB::select("SELECT * FROM tbl_credit_logs WHERE project_id = " . $project_id . " ORDER BY date desc");
+          $time = DB::select("SELECT (sum(spent) - sum(spent * discount_percent / 100)) as spent FROM tbl_credit_logs WHERE project_id = " . $project_id);
           $time = $time[0]->spent;
-          if ($time == null)
-          {
+          if ($time == null) {
                $time = 0;
           }
-          return view("pages.settings.time", array('data' => $rs,'time'=> $time)); 
+          return view("pages.settings.time", array('data' => $rs, 'time' => $time));
      }
-     public function tp_receiver_add_entry(){
-   
-     	$_POST = (json_decode(file_get_contents("php://input")));
+     public function tp_receiver_add_entry()
+     {
 
-     	$data = $_POST->Entity;
-     	
-     	$project_id = $data->ProjectID;
-     	$spent = $data->Spent;
-     	$time_id = $data->TimeID;
-     	$assignable = $data->AssignableName;
-     	$tp_user_id = $data->UserID;
+          $_POST = (json_decode(file_get_contents("php://input")));
 
-     	$entries = DB::select(" SELECT * FROM tbl_credit_logs WHERE time_id = ".$time_id);
-     	if (empty($entries))
-     	{
-     		$ins = DB::table('tbl_credit_logs')->insert(
-			    [
-			    	'date' => date('Y-m-d H:i:s'),
-			    	'project_id' => $project_id,
-			    	'spent' => '-'.$spent,
-			    	'assignable' => $assignable,
-			    	'user_id' => $tp_user_id,
-			    	'time_id' => $time_id
-			    ]
-			);
-			var_dump($ins);
-     	}
-     	else
-     	{
-	     	DB::table('tbl_credit_logs')->where('time_id', $time_id)->update(['spent' => '-'.$spent]);
-     	}
-    }
-     public function invoicing() {
+          $data = $_POST->Entity;
+
+          $project_id = $data->ProjectID;
+          $spent = $data->Spent;
+          $time_id = $data->TimeID;
+          $assignable = $data->AssignableName;
+          $tp_user_id = $data->UserID;
+
+          $entries = DB::select(" SELECT * FROM tbl_credit_logs WHERE time_id = " . $time_id);
+          if (empty($entries)) {
+               $ins = DB::table('tbl_credit_logs')->insert(
+                    [
+                         'date' => date('Y-m-d H:i:s'),
+                         'project_id' => $project_id,
+                         'spent' => '-' . $spent,
+                         'assignable' => $assignable,
+                         'user_id' => $tp_user_id,
+                         'time_id' => $time_id
+                    ]
+               );
+               var_dump($ins);
+          } else {
+               DB::table('tbl_credit_logs')->where('time_id', $time_id)->update(['spent' => '-' . $spent]);
+          }
+     }
+     public function invoicing()
+     {
           $time = DB::select("
                SELECT *,
                   (SELECT SUM(tbl_credit_logs.spent) 
@@ -65,7 +63,7 @@ class TimeEntriesController extends Controller
                    WHERE tbl_credit_logs.project_id = users.project_id) AS time
                FROM users
                ORDER By time asc");
-          return view("pages.settings.invoicing", array('time_entries'=> $time)); 
+          return view("pages.settings.invoicing", array('time_entries' => $time));
      }
-
 }
+
