9d85edb93a80698ee3b9b7354f5ffb4ed83912f4
Ronnie Isurina <ronnie.isurina@gmail.com>
2019-02-21 14:57:59 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,11 @@ Route::get("/requests", 'HomeController@requests')->name('home');
 Route::any("/home_ajax", "HomeController@home_ajax");
 Route::any("/file_upload", "HomeController@file_upload");
 
-Route::group(['middleware' => ['UserMiddleWare:canaccess']], function() {
-	Route::get("/settings", "SettingsController@index");
-	Route::any("/settings_ajax", "SettingsController@settings_ajax");
+Route::group(['middleware' => ['UserMiddleWare:canaccess']], function () {
+    Route::get("/settings", "SettingsController@index");
+    Route::any("/settings_ajax", "SettingsController@settings_ajax");
 
-	Route::get("/settings/custom", "SettingsController@custom");
-    
-    
+    Route::get("/settings/custom", "SettingsController@custom");
 });
 
 Route::get("/meeting", "MeetingsController@index");
@@ -32,8 +30,20 @@ Route::any("get_email/{id}", "NotificationsController@get_email");
 Route::any("/new_task/", "NotificationsController@NewTaskCreated");
 Route::any("time-entries/add/", "TimeEntriesController@tp_receiver_add_entry");
 Route::any("time-entries/", "TimeEntriesController@index");
-Route::any("invoicing/", "TimeEntriesController@invoicing");
+
+Route::group(['prefix' => 'invoicing'], function () {
+    Route::get("/", [
+		'uses' => 'InvoicingController@index',
+		'as' => 'invoicing.index'
+	]);
+
+    Route::get("/{project_id}", [
+        'uses' => 'InvoicingController@show',
+        'as' => 'invoicing.show'
+    ]);
+});
 #Logout
-Route::get("/logout", function(){
+Route::get("/logout", function () {
     return View::make("logout");
- });
+});
+ 
